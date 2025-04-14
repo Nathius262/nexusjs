dea5c6327fe8a39e54bb4937a53e97edf2b3b7da
Nathaniel <egbodonathaniel@yahoo.com>
2025-04-14 13:31:04 +0100

    bug fixes: remove postinstall to prevent installation error

--- src/modules/user/routes/User.routes.js
+++
@@ -1,9 +0,0 @@
-const controller = require('../controllers/User.controller');
-const express = require('express');
-const router = express.Router();
-
-router.post('/', controller.create);
-
-
-
-module.exports = router;
