70f5f47bc3ca439cc2f47a9c14491174cb650062
Nathaniel <egbodonathaniel@yahoo.com>
2025-05-16 11:09:12 +0100

    default home page

--- templates/src/config/staticFiles.js
+++ templates/src/config/staticFiles.js
@@ -13,15 +13,5 @@ const app = express();
 // Serve static files (CSS, JS, images)
 app.use('', express.static(path.join(__dirname, '..', '..', 'public')));
 app.use('/admin', express.static(path.join(__dirname, '..', '..', 'public')));
-app.use('/admin/user', express.static(path.join(__dirname, '..', '..', 'public')));
-app.use('/admin/role', express.static(path.join(__dirname, '..', '..', 'public')));
-app.use('/admin/sermon', express.static(path.join(__dirname, '..', '..', 'public')));
-app.use('/admin/zoe-record', express.static(path.join(__dirname, '..', '..', 'public')));
-app.use('/admin/nowword', express.static(path.join(__dirname, '..', '..', 'public')));
-app.use('/admin/department', express.static(path.join(__dirname, '..', '..', 'public')));
-app.use('/auth', express.static(path.join(__dirname, '..', '..', 'public')));
-app.use('/media', express.static(path.join(__dirname, '..', '..', 'public')));
-app.use('/department', express.static(path.join(__dirname, '..', '..', 'public')));
-app.use('/media/sermon', express.static(path.join(__dirname, '..', '..', 'public')));
 
 export default app;
