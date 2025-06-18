197bc37c1195ccd4f771ac7b6b3d6aa9ae4527cd
Nathaniel <egbodonathaniel@yahoo.com>
2025-06-18 15:10:52 +0100

    update index.js

--- templates/index.js
+++ templates/index.js
@@ -3,15 +3,21 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
 
 import loadModules from './src/config/load_modules.js';
 import staticFiles from "./src/config/staticFiles.js";
 import configureViewEngine from './src/config/viewEngine.js';
 import eventLogger from './src/middlewares/logger.middleware.js';
+import conditionalRendering from './src/middlewares/conditionalRender.middleware.js';
+import { adminAuthMiddleware } from './src/middlewares/adminAuth.middleware.js';
 
 // Resolve __dirname for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+
+dotenv.config();
 
 const app = express();
 
@@ -27,6 +33,13 @@ app.use(bodyParser.json());
 
 // Static files
 app.use(staticFiles);
+
+//confirm admin previlege for routes that begins with /admin...
+app.use(adminAuthMiddleware);
+
+
+//render header and footer section based on root or admin route
+app.use(conditionalRendering);
 
 // Load dynamic routes from modules
 await loadModules(app);
